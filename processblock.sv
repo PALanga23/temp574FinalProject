4053b07c44ccef2a3d15caf32afbaf31d95ea894
PALanga23 <palanga@wpi.edu>
2024-12-08 20:11:12 -0500

    Update processblock.sv

--- processblock.sv
+++ processblock.sv
@@ -1,71 +1,158 @@
-APP = poly1305FinalProject
+module processblock(input rst_ni,
+  input clk,
+  input [127:0] r,
+  input [128:0] m, // {1 padding + 128 message bits}
+  input [129:0] a_in, // {acc < P = 2^130 - 5}
+  output [129:0] a_out,
+  input start,
+  output done
+  );
+  
+  logic [130:0] m1;
+  logic [258:0] m2; // 128 bits * 131 bits = 259 bits
+  logic [131:0] m3; // first reduction leaves 2 extra bits
+  logic [129:0] m4;
+  logic [2:0] five;
+  
+  logic dutStart;
+  logic dutDone;
+  
+  assign five = 5;
+  
+  localparam IDLE = 3'd0;
+  localparam START = 3'd1;
+  localparam M1_CALC = 3'd2;
+  localparam M2_CALC = 3'd3;
+  localparam M3_CALC = 3'd4;
+  localparam AOUT_CALC = 3'd5;
+  localparam DONE = 3'd6;
+  
+  logic [2:0] state = IDLE;
+  logic [2:0] next_state = IDLE;
+  
+  // State machine to control which step is being completed
+  always_ff @(posedge clk or negedge rst_ni) begin
+    if (!rst_ni) begin
+      state <= IDLE;
+    end
+    else begin    
+      state <= next_state;
+    end
+  end
+  
+  always_comb begin   
+    //stay in current state unless indicated otherwise
+    next_state = state;
+  
+    //control logic
+    case (state)
+      IDLE: begin
+        if (start) begin
+          next_state = START;
+        end
+      end
+  
+      START: begin
+        next_state = M1_CALC;
+      end
+  
+      M1_CALC: begin
+        next_state = M2_CALC;
+      end
+      
+      M2_CALC: begin
+        if(dutDone)
+          next_state = M3_CALC;
+        end
+        else begin
+          next_state = M2_CALC;
+        end
+      end
+      
+      M3_CALC: begin
+        next_state = AOUT_CALC;
+      end
+      
+      AOUT_CALC: begin
+        next_state = DONE;
+      end
+      
+      DONE: begin
+        next_state = IDLE;
+      end
 
-all:
-	@echo "Targets:"
-	@echo "simprep          Build Verilator based system simulator"
-	@echo "swprep           Compile software (Current application: $(APP))"
-	@echo "runfst           Run Verilator and record waveformtrace"
-	@echo "run              Run Verilator"
-	@echo "syn              Prepare code for synthesis (to be done in ex-custom-obex-syn repo)"
-	@echo "clean            Remove intermediate files"
+      default: begin
+        next_state = IDLE;
+      end
+    endcase
+  end
+  
+  always_ff@(posedge clk or negedge rst_ni) begin
+    if(!rst_ni) begin
+      done <= 0;      
+      dutStart <= 0;         
+    end
+    else begin
+    
+      case(state) begin
+      
+        M2_CALC: begin
+          dutStart <= 1;
+        end
+        
+        M3_CALC: begin
+          dutStart <= 0;
+        end
+        
+        DONE: begin
+          done <= 1;
+        end
+        
+        default: begin
+          done <= 0;      
+          dutStart <= 0;   
+        end
+      endcase
+    end 
+  end
 
-build/custom_ibex_0/sim-verilator/Vtop_verilator:
-	fusesoc --cores-root=. run --target=sim --tool=verilator --setup --build custom_ibex
-
-simprep: build/custom_ibex_0/sim-verilator/Vtop_verilator
-
-sw/c/build/demo/$(APP)/$(APP):
-	mkdir -p sw/c/build
-	cd sw/c/build; cmake ..; make
-
-swprep: sw/c/build/demo/$(APP)/$(APP)
-
-runfst: build/custom_ibex_0/sim-verilator/Vtop_verilator sw/c/build/demo/$(APP)/$(APP)
-	build/custom_ibex_0/sim-verilator/Vtop_verilator -t sim.fst --meminit=ram,./sw/c/build/demo/$(APP)/$(APP)
-
-run: build/custom_ibex_0/sim-verilator/Vtop_verilator sw/c/build/demo/$(APP)/$(APP)
-	build/custom_ibex_0/sim-verilator/Vtop_verilator --meminit=ram,./sw/c/build/demo/$(APP)/$(APP)
-
-syn:
-	fusesoc --cores-root=. run --target=synth --tool=vivado --setup custom_ibex
-
-clean:
-	rm -rf build sw/c/build
-
-
-#/opt/lowrisc-toolchain-rv32imcb-20240206-1/bin/riscv32-unknown-elf-gcc  
-#-I/home/pschaumont/ex-custom-ibex/sw/c/common 
-#-march=rv32imc 
-#-mabi=ilp32 
-#-mcmodel=medany 
-#-Wall 
-#-fvisibility=hidden 
-#-ffreestanding 
-#-g 
-#-MD 
-#-MT 
-#demo/myreg-test/CMakeFiles/myreg-test.dir/main.c.obj 
-#-MF CMakeFiles/myreg-test.dir/main.c.obj.d 
-#-o CMakeFiles/myreg-test.dir/main.c.obj 
-#-c /home/pschaumont/ex-custom-ibex/sw/c/demo/myreg-test/main.c
-#
-#
-#/opt/lowrisc-toolchain-rv32imcb-20240206-1/bin/riscv32-unknown-elf-gcc 
-#-march=rv32imc 
-#-mabi=ilp32 
-#-mcmodel=medany 
-#-Wall 
-#-fvisibility=hidden 
-#-ffreestanding 
-#-g 
-#-nostartfiles 
-#-T "/home/pschaumont/ex-custom-ibex/sw/c/../common/link.ld" 
-#"CMakeFiles/myreg-test.dir/main.c.obj" 
-#../../common/CMakeFiles/common.dir/demo_system.c.obj 
-#../../common/CMakeFiles/common.dir/uart.c.obj 
-#../../common/CMakeFiles/common.dir/timer.c.obj 
-#../../common/CMakeFiles/common.dir/gpio.c.obj 
-#../../common/CMakeFiles/common.dir/pwm.c.obj 
-#../../common/CMakeFiles/common.dir/spi.c.obj 
-#../../common/CMakeFiles/common.dir/crt0.S.obj 
-#-o myreg-test
+  always_ff@(posedge clk or negedge rst_ni) begin
+    if(!rst_ni) begin
+      m1 <= 0;
+      m2 <= 0;
+      m3 <= 0;
+      m4 <= 0;
+      a_out <= 0;
+    end
+    else begin
+      case(state) begin
+        M1_CALC: begin
+          m1 <= m + a_in;
+        end
+        
+        M3_CALC: begin
+          m3 <= m2[129:0] + m2[258:130] * five; // first reduction
+        end
+        
+        AOUT_CALC: begin
+          a_out <= m3[129:0] + m3[131:130] * five; // second reduction
+        end
+        
+        DONE: begin
+          done <= 1;
+        end
+      end
+    end
+  end
+  
+  accelerator #(.BufferLength(132), .InputALength(131), .InputBLength(127)) DUT(
+    .clk_i(clk), 
+    .rst_ni(rst_ni), 
+    .inputA(m1), 
+    .inputB(r),
+    .start(dutStart),
+    .done(dutDone),
+    .outputM(m2)
+  );
+  
+endmodule
